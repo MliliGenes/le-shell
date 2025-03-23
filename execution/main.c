43abef7295584740ddc889ee61ee9713f4baac36
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-03-23 03:46:48 +0000

    libft && simple while

---
+++ execution/main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/23 03:02:50 by ssbaytri          #+#    #+#             */
+/*   Updated: 2025/03/23 03:45:36 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int exit_status = 0;
+
+void	handle_signal(int sig)
+{
+	if (sig == SIGINT)
+		write(1, "\nminishell$ ", 12);
+}
+
+void ll()
+{
+	system("leaks minishell");
+}
+
+int main()
+{
+	// atexit(ll);
+	
+	char *input;
+	
+	signal(SIGINT, handle_signal);
+	while (1)
+	{
+		input = readline("minishell$ ");
+		if (!input)
+		{
+			printf("exit\n");
+			break;
+		}
+		if (ft_strlen(input) > 0)
+			printf("%s\n", input);
+		free(input);
+	}
+	return (0);
+}
---
+++ execution/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/23 03:20:58 by ssbaytri          #+#    #+#             */
+/*   Updated: 2025/03/23 03:37:07 by ssbaytri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <readline/readline.h>
+#include "libft/libft.h"
