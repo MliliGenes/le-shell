016978a4c752e997e5f8c48d2aedae98f1d67e3c
MliliGenes <saad.elm.77@gmail.com>
2025-04-01 22:24:44 +0000

    lol the structs are fat

--- parsing /include/structs.h
+++ parsing /include/structs.h
@@ -6,7 +6,7 @@
 /*   By: sel-mlil <sel-mlil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 06:20:05 by sel-mlil          #+#    #+#             */
-/*   Updated: 2025/03/30 00:40:17 by sel-mlil         ###   ########.fr       */
+/*   Updated: 2025/04/01 22:24:28 by sel-mlil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,51 +15,131 @@
 
 # include "dependencies.h"
 
-typedef enum s_redir_type
+/**
+ * @brief Token types for lexical analysis
+ */
+typedef enum e_token_type
 {
-	ERROR,
-	REDIR_IN,
-	REDIR_OUT,
-	APPEND,
-	HEREDOC,
-}							t_redir_type;
+	TOKEN_WORD,      /* Command or argument */
+	TOKEN_PIPE,      /* Pipe operator (|) */
+	TOKEN_REDIR_IN,  /* Input redirection (<) */
+	TOKEN_REDIR_OUT, /* Output redirection (>) */
+	TOKEN_APPEND,    /* Append redirection (>>) */
+	TOKEN_HEREDOC,   /* Here document (<<) */
+	TOKEN_AND,       /* Logical AND (&&) for bonus */
+	TOKEN_OR,        /* Logical OR (||) for bonus */
+	TOKEN_PAREN_L,   /* Left parenthesis for bonus */
+	TOKEN_PAREN_R,   /* Right parenthesis for bonus */
+	TOKEN_EOF        /* End of input */
+}	t_token_type;
 
-typedef struct s_redir_token
+/**
+ * @brief Token structure
+ * @details Basic unit after lexical analysis
+ */
+typedef struct s_token
 {
-	char					*file_name;
-	t_redir_type			type;
-	struct s_redir_token	*next;
-}							t_redir_token;
+	char			*value;  /* Token string value */
+	t_token_type	type;    /* Type of the token */
+	struct s_token	*next;   /* Next token in list */
+}	t_token;
 
-typedef struct s_cmd_node
+/**
+ * @brief Lexer for tokenizing input
+ */
+typedef struct s_lexer
 {
-	char					*cmd;
-	char					*args;
-	t_redir_token			*redir;
-	struct s_cmd_node		*next;
-}							t_cmd_node;
+	char	*input;     /* Input string */
+	int		pos;        /* Current position */
+}	t_lexer;
 
-typedef struct s_op_node
+/**
+ * @brief Redirection types
+ */
+typedef enum e_redir_type
 {
-	char					*op;
-	t_redir_type			type;
-	struct s_op_node		*next;
-}							t_op_node;
+	REDIR_IN,      /* Input (<) */
+	REDIR_OUT,     /* Output (>) */
+	REDIR_APPEND,  /* Append (>>) */
+	REDIR_HEREDOC  /* Here document (<<) */
+}	t_redir_type;
 
-typedef enum s_token_type
+/**
+ * @brief Redirection structure
+ */
+typedef struct s_redir
 {
-	CMD_TOKEN,
-	OP_TOKEN,
-	L_PAREN_TOKEN,
-	R_PAREN_TOKEN,
-}							t_token_type;
+	t_redir_type	type;      /* Type of redirection */
+	char			*file;      /* Filename or delimiter */
+	struct s_redir	*next;      /* Next redirection */
+}	t_redir;
 
-typedef struct s_list
+/**
+ * @brief Simple command structure
+ */
+typedef struct s_cmd
 {
-	void					*token;
-	t_token_type			token_type;
-	struct s_list			*prev;
-	struct s_list			*next;
-}							t_list;
+	char			**args;    /* Command and its arguments */
+	t_redir			*redirs;   /* List of redirections */
+	struct s_cmd	*next;     /* For simple command lists */
+}	t_cmd;
+
+/**
+ * @brief Operator types (for bonus part)
+ */
+typedef enum e_op_type
+{
+	OP_PIPE,  /* Pipe (|) */
+	OP_AND,   /* Logical AND (&&) */
+	OP_OR     /* Logical OR (||) */
+}	t_op_type;
+
+/**
+ * @brief AST node structure
+ * @details Represents commands, pipes, and logical operators
+ */
+typedef struct s_ast
+{
+	enum {
+		AST_CMD,     /* Simple command */
+		AST_PIPE,    /* Pipeline */
+		AST_AND_OR,  /* Logical operator (&&, ||) */
+		AST_SUBSHELL /* Group in parentheses (bonus) */
+	} type;
+	
+	union {
+		t_cmd *cmd;                 /* For AST_CMD */
+		struct {
+			struct s_ast	*left;
+			struct s_ast	*right;
+			t_op_type		op;
+		} binary;                   /* For AST_PIPE and AST_AND_OR */
+		struct s_ast *subshell;     /* For AST_SUBSHELL */
+	} content;
+	
+	t_redir *redirs;               /* Redirections at this level */
+}	t_ast;
+
+/**
+ * @brief Environment variable structure
+ */
+typedef struct s_env_var
+{
+	char				*key;     /* Variable name */
+	char				*value;   /* Variable value */
+	struct s_env_var	*next;    /* Next variable */
+}	t_env_var;
+
+/**
+ * @brief Shell state structure
+ */
+typedef struct s_shell
+{
+	t_env_var	*env;          /* Environment variables */
+	int			last_status;   /* Exit status of last command ($?) */
+	char		**path;        /* PATH directories for command lookup */
+	t_ast		*ast;          /* Abstract syntax tree of current command */
+	int			running;       /* Shell running status */
+}	t_shell;
 
 #endif
