9daf7c04fd5de255798cf5b6c469fdbba986fff6
MliliGenes <saad.elm.77@gmail.com>
2025-04-16 20:26:13 +0100

    ready tokens ready time to shunt

--- parsing /include/parsing.h
+++ parsing /include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: sel-mlil <sel-mlil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 09:14:40 by sel-mlil          #+#    #+#             */
-/*   Updated: 2025/04/15 22:06:18 by sel-mlil         ###   ########.fr       */
+/*   Updated: 2025/04/16 20:15:45 by sel-mlil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # include "structs.h"
 
 # ifndef TEST
-#  define TEST "(ls -l)"
-// #  define TEST "( cat > 7arzan -e | grep \"txt\" > \"text_files\" ) && ( cat < Makefile-e)&& echo \"Text files found:\"&& cat text_files.list|| (echo \"No text files found\" >> error.log && exit 1) | wc -l < input.txt >> results.log"
+// #  define TEST "(ls -l)"
+#  define TEST "( cat > 7arzan -e | grep \"txt\" > \"text_files\" )&& ( cat < Makefile-e) && echo \"Text files found:\"&& cat text_files.list|| (echo \"No text files found\" >> error.log && exit 1) | wc -l < input.txt >> results.log"
 # endif
 
 t_lexer			*init_lexer(char *input);
@@ -51,5 +51,30 @@ int				check_parenthesis_balance(const char *input);
 bool			check_m_percent(char *input);
 int				validate_tokens(t_token *head);
 void			classify_tokens(t_token *head);
+
+t_op_type		token_to_op_type(t_token_type type);
+t_redir_type	token_to_redir_type(t_token_type type);
+
+t_redir			*create_redir_node(t_redir_type type, char *file_or_limiter);
+t_cmd			*create_cmd_node(char **args, t_redir *redirs);
+t_op			*create_op_node(t_op_type type);
+t_ready_token	*create_ready_token_node(void *token, int type);
+
+void			add_back_redir_node(t_redir **head, t_redir *node);
+void			add_back_ready_token(t_ready_token **head, t_ready_token *node);
+
+void			free_args_array(char **args, int count);
+void			free_redirs(t_redir *redirs);
+
+t_redir			*get_redirs(t_token *start, t_token *end);
+bool			has_redirections(t_token *start, t_token *end);
+int				count_args(t_token *start, t_token *end);
+char			**get_args(t_token *start, t_token *end);
+t_cmd			*extract_cmd_data(t_token *start, t_token *end);
+
+bool			process_command_segment(t_token *start, t_token *end,
+					t_ready_token **head);
+bool			add_operator_to_list(t_token *current, t_ready_token **head);
+bool			extract_tokens(t_token *tokens, t_ready_token **head);
 
 #endif
