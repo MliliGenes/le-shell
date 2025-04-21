9299aec44f722b996a175e39ab7d751d4a9991c6
ssbaytri <azjoker333@gmail.com>
2025-04-21 02:21:54 +0100

    work on cd today

--- execution/builtins/ft_cd.c
+++ execution/builtins/ft_cd.c
@@ -6,13 +6,20 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 23:08:21 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/04/17 23:08:44 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/04/21 02:01:27 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_cd(char *input)
+// Feature         | Must implement?  | Why?
+// cd ./dir        | ✅ Yes           | Relative path
+// cd /dir         | ✅ Yes           | Absolute path
+// cd .., cd .     | ⚠️ Optional       | Covered by relative logic
+// cd alone / cd ~ | ❌ No            | Not required by subject
+// cd -            | ❌ No            | Also not required
+
+void	ft_cd(char *input, t_env_var *env_list)
 {
 	
 }
