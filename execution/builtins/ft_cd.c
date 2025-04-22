cf0ae0f05dc879f9a9c79ca82ae6de3d0de222ab
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-04-22 22:48:33 +0100

    bullshit cd

--- execution/builtins/ft_cd.c
+++ execution/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 23:08:21 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/04/22 01:47:19 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/04/22 22:33:27 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,19 @@
 // cd alone / cd ~ | ❌ No            | Not required by subject
 // cd -            | ❌ No            | Also not required
 
+char *get_env_value(t_env_var *env_list, char *key)
+{
+	t_env_var *current = env_list;
+
+	while (current)
+	{
+		if (ft_strcmp(current->key, key) == 0)
+			return current->value;
+		current = current->next;
+	}
+	return NULL;
+}
+
 static void cd_error(char *path)
 {
 	ft_putstr_fd("cd: ", 2);
@@ -26,30 +39,60 @@ static void cd_error(char *path)
 	ft_putstr_fd(": No such file or directory\n", 2);
 }
 
+char	*advanced_cd(char **args, t_env_var *env_list)
+{
+	char *path;
+	
+	if (!args[1] || ft_strcmp(args[1], "~") == 0)
+	{
+		char *home;
+		
+		home = get_env_value(env_list, "HOME");
+		if (!home)
+			return (ft_putstr_fd("cd: HOME not set\n", 2), NULL);
+		path = home;
+	}
+	else if (ft_strcmp(args[1], "-") == 0)
+	{
+		char *oldpwd;
+
+		oldpwd = get_env_value(env_list, "OLDPWD");
+		if (!oldpwd)
+			return (ft_putstr_fd("cd: OLDPWD not set\n", 2), NULL);
+		printf("%s\n", oldpwd);
+		path = oldpwd;
+	}
+	else
+		path = args[1];
+	return path;
+}
+
 void	handle_cd(char *input, t_env_var *env_list)
 {
 	char **args;
+	char *path;
 	char *oldpwd;
 	char *newpwd;
 	
 	args = ft_split(input, ' ');
 	if (!args)
 		return ;
-	if (ft_strcmp(args[0], "cd") == 0 && args[1])
+	path = advanced_cd(args, env_list);
+	if (!path)
+		return (free_2d(args));
+	oldpwd = getcwd(NULL, 0);
+	if (chdir(path) == -1)
 	{
-		oldpwd = getcwd(NULL, 0);
-		if (chdir(args[1]) == -1)
-		{
-			cd_error(args[1]);
-			free(oldpwd);
-			free_2d(args);
-			return ;
-		}
+		cd_error(path);
+		free(oldpwd);
+	}
+	else
+	{
 		newpwd = getcwd(NULL, 0);
 		env_update(env_list, "OLDPWD", oldpwd);
 		env_update(env_list, "PWD", newpwd);
-		free(oldpwd);
 		free(newpwd);
 	}
+	free(oldpwd);
 	free_2d(args);
 }
