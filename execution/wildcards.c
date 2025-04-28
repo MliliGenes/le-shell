b8ef6cade304a16c3588c89e46ba9f2b3d10c73d
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-04-28 01:56:22 +0100

    TBC

--- execution/wildcards.c
+++ execution/wildcards.c
@@ -5,19 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/27 01:35:21 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/04/28 00:31:18 by ssbaytri         ###   ########.fr       */
+/*   Created: 2025/04/28 01:42:39 by ssbaytri          #+#    #+#             */
+/*   Updated: 2025/04/28 01:54:07 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	expand_wildcards(char **arg)
+static int match(char *filename, char *pattern)
+{
+	(void)filename;
+	if (ft_strcmp(pattern, "*") == 0)
+		return (1);
+	if (pattern[0] == '*' && pattern[ft_strlen(pattern) - 1] != '*')
+	{
+		char *text = pattern + 1;
+		int len = ft_strlen(text);
+		int file_len = ft_strlen(filename);
+		if (file_len >= len && ft_strcmp(filename + file_len - len, text) == 0)
+			return (1);
+	}
+	else if (pattern[ft_strlen(pattern) - 1] == '*' && pattern[0] != '*')
+    {
+        int len = ft_strlen(pattern) - 1;
+        if (ft_strncmp(filename, pattern, len) == 0)
+            return (1);
+    }
+	return (0);
+}
+
+void	expand_full(char **arg)
 {
 	(void)arg;
-	DIR			*dir;
+	DIR *dir;
 	struct dirent *entry;
-
+	
 	dir = opendir(".");
 	if (!dir)
 	{
@@ -27,7 +49,9 @@ void	expand_wildcards(char **arg)
 	entry = readdir(dir);
 	while (entry)
 	{
-		printf("%s\n", entry->d_name);
+		if (ft_strcmp(entry->d_name, ".") != 0 && ft_strcmp(entry->d_name, "..") != 0 
+			&& match(entry->d_name, *arg))
+			printf("%s\n", entry->d_name);
 		entry = readdir(dir);
 	}
 	closedir(dir);
@@ -44,8 +68,8 @@ void handle_wildcards(char *input)
 		return;
 	while (args[i])
 	{
-		if (ft_strchr(args[i], '*'))
-			expand_wildcards(&args[i]);
+		if (ft_strchr(args[i], '*') != NULL)
+			expand_full(&args[i]);
 		i++;			
 	}
 	free_2d(args);
