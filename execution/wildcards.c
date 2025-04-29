515334e6cc362f54ae40d4f6358166f34994c257
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-04-29 18:17:29 +0100

    hopefully it's working

--- execution/wildcards.c
+++ execution/wildcards.c
@@ -6,18 +6,50 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 01:42:39 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/04/28 01:54:07 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/04/29 18:15:45 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int ft_counter(char *str, char c)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+int match_split(char *filename, char **pieces)
+{
+	int i = 0;
+	char *found;
+
+	while (pieces[i])
+	{
+		found = ft_strnstr(filename, pieces[i], ft_strlen(filename));
+		if (!found)
+			return (0);
+		filename = found + ft_strlen(pieces[i]);
+		i++;
+	}
+	return (1);
+}
 
 static int match(char *filename, char *pattern)
 {
 	(void)filename;
 	if (ft_strcmp(pattern, "*") == 0)
 		return (1);
-	if (pattern[0] == '*' && pattern[ft_strlen(pattern) - 1] != '*')
+	if (pattern[0] == '*' && pattern[ft_strlen(pattern) - 1] != '*' && ft_counter(pattern, '*') == 1)
 	{
 		char *text = pattern + 1;
 		int len = ft_strlen(text);
@@ -25,12 +57,36 @@ static int match(char *filename, char *pattern)
 		if (file_len >= len && ft_strcmp(filename + file_len - len, text) == 0)
 			return (1);
 	}
-	else if (pattern[ft_strlen(pattern) - 1] == '*' && pattern[0] != '*')
+	else if (pattern[ft_strlen(pattern) - 1] == '*' && pattern[0] != '*' && ft_counter(pattern, '*') == 1)
     {
         int len = ft_strlen(pattern) - 1;
         if (ft_strncmp(filename, pattern, len) == 0)
             return (1);
     }
+	else if (pattern[0] == '*' && pattern[ft_strlen(pattern) - 1] == '*' && ft_counter(pattern, '*') == 2)
+	{
+		char *text = ft_substr(pattern, 1, ft_strlen(pattern) - 2);
+		if (!text)
+			return (0);
+		if (ft_strnstr(filename, text, ft_strlen(filename)))
+		{
+			free(text);
+			return (1);
+		}
+		free(text);
+	}
+	else
+	{
+		char **split = ft_split(pattern, '*');
+		if (!split)
+			return (0);
+		if (match_split(filename, split))
+		{
+			free_2d(split);
+			return (1);
+		}
+		free_2d(split);
+	}
 	return (0);
 }
 
