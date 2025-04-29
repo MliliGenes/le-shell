92a695a216d51937e4ff722081d9ce14c2c4470e
MliliGenes <saad.elm.77@gmail.com>
2025-04-29 11:49:47 +0100

    this branch will have everything

--- test.c
+++ test.c
@@ -10,7 +10,6 @@ typedef struct var
 	struct var	*next;
 }				var;
 
-// Utility function to create a new var node
 var	*create_var(const char *key, const char *value)
 {
 	var	*new_var;
@@ -24,7 +23,6 @@ var	*create_var(const char *key, const char *value)
 	return (new_var);
 }
 
-// Function to free the linked list
 void	free_var_list(var *head)
 {
 	var	*current;
@@ -76,153 +74,166 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (ptr);
 }
 
-int count_expended_len(char *input,int initial_len, var *vars)
+typedef struct s_expansion
 {
-	int		i;
-	int in_q;
-	int		start;
-	char	*value;
+	char		*input;
+	char		*output;
+	int			s_quote;
+	int			d_quote;
+	int			i_index;
+	int			o_index;
+	int			len;
+	var			*vars;
+}				t_expansion;
 
-	i = 0;
-	in_q = 0;
-	while (input[i])
-	{
-		if (input[i] == '\'')
-		{
-			in_q = !in_q;
-		}
-		else if (!in_q && input[i] == '$' && (isalpha(input[i + 1]) || input[i + 1] == '_'))
-		{
-			i++;
-			start = i;
-			initial_len--;
-			while (input[i] && (isalnum(input[i]) || input[i] == '_'))
-			{
-				initial_len--;
-				i++;
-			}
-			value = find_env_var(vars, ft_substr(input, start, i - start));
-			if (value)
-				initial_len += strlen(value);
-			continue ;
-		}
-		i++;
-	}
-	return (initial_len);
+void	init_expansion(t_expansion *exp, char *input, var *vars)
+{
+	exp->input = input;
+	exp->len = strlen(input);
+	exp->s_quote = 0;
+	exp->d_quote = 0;
+	exp->i_index = 0;
+	exp->o_index = 0;
+	exp->vars = vars;
 }
 
+void	update_quote_state(t_expansion *exp, char current_char)
+{
+	if (current_char == '\'' && !exp->d_quote)
+		exp->s_quote = !exp->s_quote;
+	if (current_char == '\"' && !exp->s_quote)
+		exp->d_quote = !exp->d_quote;
+}
+
+void    process_variable_length(t_expansion *exp)
+{
+    int     start;
+    char    *key;
+    char    *value;
+    
+    exp->i_index++;
+    start = exp->i_index;
+    exp->len--;
+    while (exp->input[exp->i_index] && 
+           (isalnum(exp->input[exp->i_index]) || exp->input[exp->i_index] == '_'))
+    {
+        exp->len--;
+        exp->i_index++;
+    }
+    key = ft_substr(exp->input, start, exp->i_index - start);
+    value = find_env_var(exp->vars, key);
+    if (value)
+        exp->len += strlen(value);
+    free(key);
+}
+
+void	calculate_expanded_length(t_expansion *exp)
+{
+    while (exp->input[exp->i_index])
+    {
+        update_quote_state(exp, exp->input[exp->i_index]);
+        
+        if (!exp->s_quote && exp->input[exp->i_index] == '$' && 
+            (isalpha(exp->input[exp->i_index + 1]) || exp->input[exp->i_index + 1] == '_'))
+            process_variable_length(exp);
+        else
+            exp->i_index++;
+    }
+    
+    exp->i_index = 0;
+}
+
+char	*extract_var_name(t_expansion *exp)
+{
+	int	start;
+	int	length;
+
+	start = exp->i_index;
+	length = 0;
+	while (exp->input[exp->i_index]
+		&& (isalnum(exp->input[exp->i_index])
+			|| exp->input[exp->i_index] == '_'))
+	{
+		exp->i_index++;
+		length++;
+	}
+	return (ft_substr(exp->input, start, length));
+}
+
+void	expand_variable(t_expansion *exp)
+{
+	char	*key;
+	char	*value;
+	int		value_index;
+
+	value_index = 0;
+	exp->i_index++;
+	key = extract_var_name(exp);
+	value = find_env_var(exp->vars, key);
+	if (value)
+	{
+		while (value[value_index])
+			exp->output[exp->o_index++] = value[value_index++];
+	}
+	free(key);
+}
 
 void	expand_vars(char *input, var *vars)
 {
-	int		total_len;
-	int		i;
-	int		in_q = 0;
-	int		in_dq = 0;
-	int		start;
-	char	*key;
-	char	*value;
-	char	*new_input;
-	int		j;
-	int		k;
+	t_expansion	exp;
 
-	i = 0;
-	total_len = strlen(input);
-	while (input[i])
+	init_expansion(&exp, input, vars);
+	calculate_expanded_length(&exp);
+	
+	exp.output = (char *)malloc(exp.len + 1);
+	if (!exp.output)
+		return;
+	while (input[exp.i_index])
 	{
-		if (input[i] == '\'' && !in_dq)
-		{
-			in_q = !in_q;
-		}
-		if (input[i] == '\"' && !in_q)
-		{
-			in_dq = !in_dq;
-		}
-		if (!in_q && input[i] == '$' && (isalpha(input[i + 1]) || input[i + 1] == '_'))
-		{
-			i++;
-			start = i;
-			total_len--;
-			while (input[i] && (isalnum(input[i]) || input[i] == '_'))
-			{
-				total_len--;
-				i++;
-			}
-			key = ft_substr(input, start, i - start);
-			value = find_env_var(vars, key);
-			if (value)
-				total_len += strlen(value);
-			continue ;
-		}
-		i++;
-	}
-	new_input = malloc(total_len + 1);
-	i = 0;
-	j = 0;
-	k = 0;
-	in_q = 0;
-	while (input[i])
-	{
-		if (input[i] == '\'' && !in_dq)
-		{
-			in_q = !in_q;
-		}
-		if (input[i] == '\"' && !in_q)
-		{
-			in_dq = !in_dq;
-		}
-		if (!in_q && input[i] == '$' && (isalpha(input[i + 1]) || input[i + 1] == '_'))
-		{
-			k = i + 1;
-			start = k;
-			while (input[k] && (isalnum(input[k]) || input[k] == '_'))
-				k++;
-			key = ft_substr(input, start, k - start);
-			i += strlen(key) + 1;
-			value = find_env_var(vars, key);
-			k = 0;
-			while (value && value[k])
-				new_input[j++] = value[k++];
-			
-		}
+		update_quote_state(&exp, input[exp.i_index]);
+		
+		if (!exp.s_quote && input[exp.i_index] == '$'
+			&& (isalpha(input[exp.i_index + 1]) || input[exp.i_index + 1] == '_'))
+			expand_variable(&exp);
 		else
-		{
-			new_input[j++] = input[i++];
-		}
+			exp.output[exp.o_index++] = input[exp.i_index++];
 	}
-	new_input[j] = 0;
+	exp.output[exp.o_index] = 0;
 	printf("before: %s\n", input);
-	printf("after: %s\n", new_input);
+	printf("after: %s\n", exp.output);
+	free(exp.output);
 }
 
 int	main(void)
 {
-	// Create several variables for testing
-	var *env_vars = create_var("HOME", "/home/user");
-	var *current = env_vars;
+	var		*env_vars;
+	var		*current;
+	char	*test1;
+	char	*test2;
+	char	*test3;
+	char	*test4;
+	char	*test5;
 
+	env_vars = create_var("HOME", "/home/user");
+	current = env_vars;
 	current->next = create_var("USER", "johndoe");
 	current = current->next;
-
+	current->next = create_var("VAR", "s -al");
+	current = current->next;
 	current->next = create_var("PATH", "/usr/bin:/bin");
 	current = current->next;
-
 	current->next = create_var("SHELL", "/bin/bash");
 	current = current->next;
-
 	current->next = create_var("TERM", "xterm");
 	current = current->next;
-
 	current->next = create_var("PWD", "/home/user/projects");
 	current = current->next;
-
 	current->next = create_var("LANG", "en_US.UTF-8");
-
-	char *test1 = "$USER \"'$HOME'\"";
-	char *test2 = "Current directory: $PWD using $SHELL";
-	char *test3 = "Missing variable: $NONEXISTENT should be skipped";
-	char *test4 = "Multiple variables: $USER is using $TERM in $HOME";
-	char *test5 = "$VAR at the beginning and at the end $PWD";
-
+	test1 = "l$VAR";
+	test2 = "Current directory: $PWD using $SHELL";
+	test3 = "Missing variable: $NONEXISTENT should be skipped";
+	test4 = "Multiple variables: $USER is using $TERM in $HOME";
+	test5 = "$VAR at the beginning and at the end $PWD";
 	printf("Environment Variables:\n");
 	current = env_vars;
 	while (current)
@@ -231,18 +242,12 @@ int	main(void)
 		current = current->next;
 	}
 	printf("\n");
-
 	printf("Test Strings:\n");
-
-	// TODO: Call your expand_vars function here and print the results
 	expand_vars(test1, env_vars);
 	expand_vars(test2, env_vars);
 	expand_vars(test3, env_vars);
-
-	// Clean up
+	expand_vars(test4, env_vars);
+	expand_vars(test5, env_vars);
 	free_var_list(env_vars);
-
 	return (0);
 }
-
-//	execution steps open in files if the fd for in is not -1 go ahead and open the out files if the out file fd not -1 continue to prepering the redirs
